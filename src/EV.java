d1c0e323dc7821d1f5b08e0c65721d325fafab0d
Sergejus <Sergejus.balciunas@gmail.com>
2024-04-18 19:48:32 +0300

    NKA ie EV su AutomobiliuDuomenuModifikatorius abstrack klase

---
+++ src/EV.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class EV extends AutomobiliuDuomenuModifikatorius{
+
+    private Scanner scanner = new Scanner(System.in);
+    private static String ERROR = "Ivyko Klaida, bandykite dar karta.";
+
+    @Override
+    public void pakeistiParametrus(Automobilis automobilis) {
+        System.out.println("Iveskite kuro vartojima 100 km:");
+        ((NaftosKuroAutomobilis) automobilis).setDegaluVartojimas(nuskanuotiIntVerte());
+    }
+
+    private int nuskanuotiIntVerte(){
+        int i;
+        try {
+            i = Integer.parseInt(nuskaityti());
+        } catch (NumberFormatException e) {
+            System.out.println(ERROR);
+            return nuskanuotiIntVerte();
+        }
+        return i;
+    }
+
+    private String nuskaityti(){
+        String verte = null;
+        try {
+            verte = scanner.nextLine();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return verte;
+    }
+
+}
+
+
